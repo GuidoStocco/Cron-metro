ef8956edc180b2505588bda5683cd35b5bdba8ff
unknown <guidostoccodev@gmail.com>
2024-08-27 19:42:14 -0300

    feat: adicionando o TouchableOpacity e css

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React, {useState} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 
 export default function App(){
   return(
    <View style={styles.container}>
-      <Text>Guido</Text>
+      <Image source={require('./src/crono.png')}/>
+
+      <Text style={styles.timer}>00:00:00</Text>
+
+      <View style={styles.btnArea}>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnTexto}>Iniciar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnTexto}>Limpar</Text>
+        </TouchableOpacity>
+      </View>
    </View>
   );
 };
@@ -15,7 +27,33 @@ const styles = StyleSheet.create({
   container:{
     flex: 1,
     justifyContent:'center',
-    alignItems:'center'
+    alignItems:'center',
+    backgroundColor: '#00aeef'
+  },
+  timer: {
+    marginTop: -160,
+    fontWeight: 'bold',
+    fontSize: 45,
+    color: '#fff'
+  },
+  btnArea: {
+    flexDirection: 'row',
+    marginTop: 130,
+    height: 40,
+  },
+  btn:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    margin: 17,
+    height: 40,
+    borderRadius:10
+  },
+  btnTexto: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#00aeef'
   }
 })
 
