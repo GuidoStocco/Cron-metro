e29e9d02c00ddc0969a69f65e5ebf4852de36982
unknown <guidostoccodev@gmail.com>
2024-08-27 19:22:02 -0300

    feat: setup inicial

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+export default function App(){
+  return(
+   <View style={styles.container}>
+      <Text>Guido</Text>
+   </View>
   );
-}
+};
 
+//
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent:'center',
+    alignItems:'center'
+  }
+})
+
+
+
+
